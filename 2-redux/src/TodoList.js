6d99cd9c3bf859978b254c550339af0373ee1fd1
Jonathan Chen <jonchen@jonchen-macbookpro2.roam.corp.google.com>
2019-07-14 01:17:22 -0400

    Removed unused PropTypes

--- 2-redux/src/TodoList.js
+++ 2-redux/src/TodoList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import './TodoList.css';
 import { addTodo, removeTodo } from './store/actions'
